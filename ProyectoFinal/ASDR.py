257d01a2783337aba8937d92ee791ed51db2ac6d
GustavoRojas616 <146793790+GustavoRojas616@users.noreply.github.com>
2024-01-03 20:01:48 -0600

    Update ASDR.py
    Definición de los tipos de token

--- ProyectoFinal/ASDR.py
+++ ProyectoFinal/ASDR.py
@@ -155,7 +155,52 @@ class StmtVar:
 
     def __str__(self):
         return f"StmtVar({self.name}{self.initializer})"
+class TipoToken:
+    #Tokens de un solo caracter
+    LEFT_PAREN = 'LEFT_PAREN'
+    RIGHT_PAREN = 'RIGHT_PAREN'
+    LEFT_BRACE = 'LEFT_BRACE'
+    RIGHT_BRACE = 'RIGHT_BRACE'
+    COMMA = 'COMMA'
+    DOT = 'DOT'
+    MINUS = 'MINUS'
+    PLUS = 'PLUS'
+    SEMICOLON = 'SEMICOLON'
+    SLASH = 'SLASH'
+    STAR = 'STAR'
 
+    #Tokens de uno o dos caracteres
+    BANG = 'BANG'
+    BANG_EQUAL = 'BANG_EQUAL'
+    EQUAL = 'EQUAL'
+    EQUAL_EQUAL = 'EQUAL_EQUAL'
+    GREATER = 'GREATER'
+    GREATER_EQUAL = 'GREATER_EQUAL'
+    LESS = 'LESS'
+    LESS_EQUAL = 'LESS_EQUAL'
+
+    #Literales
+
+    IDENTIFIER = 'IDENTIFIER'
+    STRING = 'STRING'
+    NUMBER = 'NUMBER'
+
+    #Palabras clave
+    AND = 'AND'
+    ELSE = 'ELSE'
+    FALSE = 'FALSE'
+    FUN = 'FUN'
+    FOR = 'FOR'
+    IF = 'IF'
+    NULL = 'NULL'
+    OR = 'OR'
+    PRINT = 'PRINT'
+    RETURN = 'RETURN'
+    TRUE = 'TRUE'
+    VAR = 'VAR'
+    WHILE = 'WHILE'
+
+    EOF = 'EOF'
 
 class ASDR:
     global tokens
